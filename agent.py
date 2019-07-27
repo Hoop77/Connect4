64b1252202df05f8ce9397edfdec4a935726dc3c
Henning <hest1620@th-wildau.de>
2019-07-27 15:49:25 +0200

    Moved methods from train to agent

--- agent.py
+++ agent.py
@@ -7,6 +7,13 @@ from keras.optimizers import Adam, SGD
 import board
 import math
 
+REWARD_TABLE = {
+	board.OUTCOME_NONE: 0.0,
+	board.OUTCOME_WIN: 1.0,
+	board.OUTCOME_DEFEAT: -1.0,
+	board.OUTCOME_DRAW: 0.5
+}
+
 class Agent:
     def __init__(self,
                  gamma=0.95,
@@ -44,6 +51,48 @@ class Agent:
         stats['loss']['values'].append(np.sqrt(history.history['loss'][-1]))
         
         self.total_steps += 1
+
+    def self_play(self,
+              epsilon=0.1,
+              gamma=0.9,
+              stats=None):
+        episode_length = 0
+        state = np.zeros(board.STATE_SHAPE)
+        player = board.PLAYER_1
+        outcome = board.OUTCOME_NONE
+        while outcome == board.OUTCOME_NONE:
+            random_move = np.random.rand() <= epsilon
+            if random_move:
+                next_state = board.make_random_move(state, player)
+                outcome = board.get_outcome_after_move(next_state, player)
+                if outcome == board.OUTCOME_WIN or outcome == board.OUTCOME_DEFEAT:
+                    target = REWARD_TABLE[outcome]
+                    self.train(state, target, stats=stats)
+            else:
+                next_state, target = self.make_best_move(state, player, gamma)
+                self.train(state, target, stats=stats)
+
+            outcome = board.get_outcome_after_move(next_state, player)
+            state = next_state
+            player = -player
+            episode_length += 1
+        
+        stats['episode_outcomes'].append(-player)
+        stats['episode_lengths'].append(episode_length)
+
+    def make_best_move(self, state, player, gamma):
+        cols = board.get_free_columns(state)
+        next_states = [board.drop_piece(state, col, player) for col in cols]
+        outcomes = [board.get_outcome_after_move(next_state, player) for next_state in next_states]
+        rewards = np.array([REWARD_TABLE[outcome] for outcome in outcomes])
+        not_done = np.array([outcome == board.OUTCOME_NONE for outcome in outcomes]).astype(np.float32)
+        values = np.squeeze(self.model.predict(np.expand_dims(next_states, axis=3)))
+        targets = rewards + not_done * gamma * values
+        targets = player * targets
+        target_max_idx = np.argmax(targets)
+        target_max = targets[target_max_idx]
+        best_next_state = next_states[target_max_idx]
+        return best_next_state, player * target_max
 
     def load(self, path):
         self.model.load_weights(path)
